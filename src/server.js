c9eae94f953eac8c45e284421045367ff0e8a7ab
emzei <emzei.j@gmail.com>
2022-09-23 04:05:33 +0900

    fix for heroku

--- src/server.js
+++ src/server.js
@@ -4,23 +4,27 @@ import { ApolloServer } from "apollo-server-express";
 import { typeDefs, resolvers } from "./schema";
 import { getUsers, protectedResolver } from "./users/users.utils";
 import graphqlUploadExpress from "graphql-upload/graphqlUploadExpress.js";
+import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
 import logger from "morgan";
 
 const PORT = process.env.PORT;
 const startServer = async () => {
   const app = express();
-  app.use(logger('tiny'));
+  app.use(logger("tiny"));
   app.use(graphqlUploadExpress());
   app.use("/static", express.static("uploads"));
   const server = new ApolloServer({
     typeDefs,
     resolvers,
+    playground: true,
+    plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
     context: async ({ req }) => {
       return {
         loggedInUser: await getUsers(req.headers.token),
         protectedResolver,
       };
     },
+    introspection:true,
   });
   await server.start();
   server.applyMiddleware({ app });
